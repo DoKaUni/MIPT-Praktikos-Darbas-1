231883b7cf515569f8a33a6d62db5ba3860f158f
DoKaUni <dovydas.kasulis@gmail.com>
2023-09-21 20:36:45 +0300

    Added another TextView & a corresponding Set Text Button

--- app/src/main/java/com/example/miptpraktikosdarbas1/MainActivity.java
+++ app/src/main/java/com/example/miptpraktikosdarbas1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.miptpraktikosdarbas1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                TextView Text2 = findViewById(R.id.textView2);
+
+                Text2.setText("Text Successfully Set");
+            }
+        });
     }
 }
